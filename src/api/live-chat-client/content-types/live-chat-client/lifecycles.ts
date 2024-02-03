6ce892431072afa7bf1f6ea689fd46522694baf6
bn-ilya <bn.ilyaa@yandex.ru>
2024-02-03 15:21:26 +0300

    refactor: chang func random4Digit

--- src/api/live-chat-client/content-types/live-chat-client/lifecycles.ts
+++ src/api/live-chat-client/content-types/live-chat-client/lifecycles.ts
@@ -20,12 +20,6 @@ async function generateCode() {
   return code;
 }
 
-
 function random4Digit(){
-  return shuffle( "0123456789".split('') ).join('').substring(0,4);
-}
-
-function shuffle(o){
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
+  return Math.floor(1000 + Math.random() * 9000);
 }
