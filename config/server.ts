982ef1dae88e9a98c7c80836b6e364ebc93a6d8f
bn-ilya <bn.ilyaa@yandex.ru>
2024-02-29 06:47:22 +0300

    feat: add app keys

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ["kjQCN4L971Fy/MZ5Z/QbRQ==", "zj4zlXF3r13ryyCDm8Wzzw==", "vV37R7Voe9G28dbUxcC67Q==", "AncBkXp46GW2JeW9Ym3Mgw=="],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
